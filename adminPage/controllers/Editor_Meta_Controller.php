23e1042f8323d2cf2cd6aa8c3af4a01e5d3f2168
Brenda <57791674+BrendaSpencer@users.noreply.github.com>
2024-11-08 11:26:32 +0100

    setting up the controller for the meta data editor

---
+++ adminPage/controllers/Editor_Meta_Controller.php
@@ -0,0 +1,19 @@
+<?php 
+declare(strict_types=1);
+
+namespace WSPEC\adminPage\Controllers;
+
+use WSPEC\adminPage\Views\Editor_Meta_Variable_Input;
+use WSPEC\adminPage\Views\Editor_Meta_Simple_Input;
+
+class Editor_Meta_Controller {
+
+    public function __construct() {
+		error_log('=== in constructor meta data ===');
+		$Variable_input = new Editor_Meta_Variable_Input();
+		$Simple_input = new Editor_Meta_Simple_Input();
+		add_action('woocommerce_product_after_variable_attributes', [$Variable_input, 'render_Variable_fields'], 9, 3);
+		add_action('woocommerce_save_product_variation',[$Variable_input,'save_variables'], 11,2);
+		add_action('woocommerce_process_product_meta',[$Simple_input,'save_variables'], 11,2);
+    }
+}
--- includes/Plugin.php
+++ includes/Plugin.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace WSPEC\includes;
 
-use WSPEC\adminPage\controllers\Editor_tab_controller;
+use WSPEC\adminPage\Controllers\Editor_Tab_Controller;
+use WSPEC\adminPage\Controllers\Editor_Meta_Controller;
 use WSPEC\includes\Enqueue_Scripts;
 use WSPEC\includes\Enqueue_Styles;
 
 class Plugin {
 
     public function __construct() {
-       
+     
         add_action('plugins_loaded', [$this, 'initialize_plugin']);
     }
 
@@ -22,7 +23,7 @@ class Plugin {
     }
 
     public function activate() {
-  
+        
         $this->enqueue_extender_scripts();
         $this->enqueue_extender_styles();
     }
@@ -36,7 +37,8 @@ class Plugin {
     }
 
     public function create_editor_communicator_admin_classes() {
+		new Editor_Meta_Controller();
         
-        new Editor_tab_controller();
+        new Editor_Tab_Controller();
     }
 }
